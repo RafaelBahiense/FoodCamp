640812a0457a9714c78f9a0a43dbf38ff3df28ae
RafaelBahiense <rafaelbahiense@id.uff.br>
2021-04-05 15:48:06 -0300

    Checkout calc

--- scripts/script.js
+++ scripts/script.js
@@ -11,12 +11,10 @@ function removeClass(elementList) {
 function select(id, category, selected) {
     if (selected === true) {
         const elementList = document.querySelectorAll(category);
-        console.log(elementList)
         removeClass(elementList);
     }
     const element = document.getElementById(id);
     element.classList.add("selected");
-    console.log(selected)
 }
 
 function foodSelect(id) {
@@ -48,6 +46,27 @@ function checkoutButton() {
 
 // Overlay
 function checkoutToggle() {
+    buildCheckout()
     const element = document.querySelector(".overlay");
     element.classList.toggle("finish-order");
+}
+
+// Checkout
+function buildCheckout() {
+    const itemsList = document.querySelectorAll(".selected .item-name");
+    let itemsListCheckout = document.querySelectorAll(".checkout-name");
+    const pricesList = document.querySelectorAll(".selected .item-price");
+    let pricesListCheckout = document.querySelectorAll(".checkout-price");
+    let total = 0;
+    for (var i = 0; i < itemsList.length; i++) {
+        itemsListCheckout[i].innerHTML = itemsList[i].innerHTML;
+        pricesListCheckout[i].innerHTML = pricesList[i].innerHTML;
+        let str = pricesListCheckout[i].innerHTML
+        str = str.replace("R$ ", "");
+        str = str.replace(",", ".");
+        total += parseFloat(str);
+        console.log(total);
+    }
+    total = (Math.round((total) * 10) / 10).toFixed(2);;
+    document.querySelector(".total").innerHTML = total;
 }
